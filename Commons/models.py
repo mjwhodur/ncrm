b4b58df3630ae1059f605475690f2a1564ac3f44
Michal Weglowski-Hodur <mjwhodur@gmail.com>
2018-10-18 11:31:52 +0200

    Documentation stubs and performance measures

--- Commons/models.py
+++ Commons/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
-class Document(models.Model):
-    pass
+# Create your models here.
 
 class Company(models.Model):
     pass
 
+
+class Document(models.Model):
+    pass
+
+
 class Item(models.Model):
     pass
 
+
 class ItemRecord(models.Model):
     pass
-
-
-
