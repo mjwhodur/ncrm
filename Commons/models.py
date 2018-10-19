49bd69bf1d7002022f174c69351bbdfdc8ff5d9f
Michal Weglowski-Hodur <mjwhodur@gmail.com>
2018-10-19 11:34:32 +0200

    Started modules MyClient and Home

--- Commons/models.py
+++ Commons/models.py
@@ -4,15 +4,23 @@ from django.db import models
 # Create your models here.
 
 class Company(models.Model):
-    pass
+    """
+        Holds data about Company issuing document. Note, that documents are system-wide.
+    """
+    IdentificationNumber = models.CharField(max_length=50)
+    TaxID = models.CharField(max_length=50)
 
+class Divisions(models.Model):
+    pass
 
 class Document(models.Model):
     pass
 
-
-class Item(models.Model):
+class DocumentSettlement(models.Model):
     pass
+
+class ItemTypes(models.Model):
+    Name = models.CharField(max_length=50)
 
 
 class ItemRecord(models.Model):
