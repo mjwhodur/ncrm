59de8c8593495ff2c058f6cd6e0caddc5a10dc35
Michal Weglowski-Hodur <mjwhodur@gmail.com>
2018-10-19 11:51:53 +0200

    Added landing stub

--- NCRM/urls.py
+++ NCRM/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import Home.urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', Home.urls)
 ]
