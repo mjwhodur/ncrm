66b5392812c3e9e79e803f718c451e763cd76b44
Michal Weglowski-Hodur <mjwhodur@tutanota.de>
2018-10-21 19:05:46 +0200

    Modele serwisu

--- ITService/models.py
+++ ITService/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    Name = models.CharField(max_length=50)
+    Mnemonic = models.CharField(max_length=5)
+
+
+class Project(models.Model):
+    Name = models.CharField(max_length=500)
+    Category = models.ForeignKey(Category, on_delete=models.deletion.DO_NOTHING)
+    Description = models.TextField(verbose_name='Opis')
+
+
+class Severity(models.Model):
+    Level = models.IntegerField(default=0)
+    Name = models.CharField(max_length=50, blank=False)
+
+
+class Patch(models.Model):
+    Number = models.CharField(max_length=500)
+    Name = models.CharField(max_length=500)
+
+
+class Issue(models.Model):
+    Number = models.CharField(max_length=500)
+    Name = models.CharField(max_length=500)
+    Description = models.TextField(max_length=65536)
+
+
+class IssuePatch(models.Model):
+    Issue = models.ForeignKey(Issue, on_delete=models.deletion.CASCADE)
+    Patch = models.ForeignKey(Patch, on_delete=models.deletion.CASCADE)
+
+
+class IssueAttachments(models.Model):
+    Issue = models.ForeignKey(Issue, on_delete=models.deletion.CASCADE)
--- bookkeeping/models.py
+++ bookkeeping/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    Name = models.CharField(max_length=50)
+    Mnemonic = models.CharField(max_length=5)
+
