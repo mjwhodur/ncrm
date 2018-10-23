724158338392a7a706b0ed8de8114f8c22cca5b5
Michal Weglowski-Hodur <mjwhodur@tutanota.de>
2018-10-23 12:09:04 +0200

    Added locale

--- Home/views.py
+++ Home/views.py
@@ -2,4 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing(request):
-    return render(request, template_name='home/landing.html')
+    if request.user.is_aunthenticated():
+        return render(request, template_name='home/panel.html')
+    else:
+        return render(request, template_name='home/landing.html')
+
+def login(request): # TODO Comment
+    """
+
+    :param request:
+    :return:
+    """
+    pass
+
+def logout(request): # TODO COMMENT
+    """
+
+    :param request:
+    :return:
+    """
+    pass
+
+def change_password(request): #TODO COMMENT
+    """
+
+    :param request:
+    :return:
+    """
+    pass
+
--- NCRM/settings.py
+++ NCRM/settings.py
@@ -104,8 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'pl-pl'
+LOCALE_PATHS = [
+    'locale/'
+]
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
